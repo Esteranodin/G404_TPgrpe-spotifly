2730386e633e60897b2016b787fa6a5e55e47da2
naimdeb <naimdebard02@gmail.com>
2024-12-19 11:21:01 +0100

    fixed header flex

--- public/components/header.php
+++ public/components/header.php
@@ -1,27 +1,26 @@
 <header>
-    <section class="relative h-[100px] bg-primary-grey-dark flex font-title">
+    <section class="relative h-[100px] bg-primary-grey-dark flex flex-row font-title">
+
         <a href="./homepage.php" class="w-[100px] flex-none">
             <img src="../assets/src/images/logo/Spotifly_Logo-sans_fond.png" alt="Logo Spotifly" class=" h-full self-center">
         </a>
 
-        <nav class="max-sm:absolute flex right-2 gap-4 bottom-3.5 p-4 items-center sm:flex-1">
+        <nav class="max-sm:absolute lg:absolute flex right-2 gap-4 bottom-3.5 p-4 items-center sm:flex-1 lg:w-[75vw] lg:justify-between">
 
             <!-- Searchbar -->
 
-            <div class="searchbar sm:bg-neutral-black sm:py-2 sm:px-4 rounded-full sm:flex items-center sm:flex-1">
-            <input type="text" placeholder="Recherchez une musique" class="text-neutral-white bg-neutral-black max-sm:hidden sm:flex-1 sm:w-[115%]">
-            <i class="fas fa-magnifying-glass text-neutral-white text-3xl sm:text-2xl"></i>
-            
+            <div class="searchbar sm:bg-neutral-black sm:py-2 sm:px-4 rounded-full sm:flex items-center sm:flex-1 lg:flex-none  lg:w-[50vw]">
+                <input type="text" placeholder="Recherchez une musique" class="text-neutral-white bg-neutral-black max-sm:hidden sm:flex-1">
+                <i class="fas fa-magnifying-glass text-neutral-white text-3xl sm:text-2xl"></i>
             </div>
-            <!-- User profile -->
-            <i class="fas fa-user text-neutral-white text-3xl"></i>
-            <!-- Burger Menu -->
-            <i class="fas fa-bars text-neutral-white text-3xl"></i>
 
-
-
-
-
+            <div class="flex gap-4">
+                <!-- User profile -->
+                <i class="fas fa-user text-neutral-white text-3xl"></i>
+                <!-- Burger Menu -->
+                <i class="fas fa-bars text-neutral-white text-3xl"></i>
+            </div>
         </nav>
+
     </section>
 </header>
